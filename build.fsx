c3e81dd621bff0e71bab8a738e9ecd8bc47cc995
Enrique Alfonso Casanovas Pedre <eacasanovaspedre@outlook.com>
2018-09-17 11:56:02 -0300

    Initial commit

---
+++ build.fsx
@@ -0,0 +1,127 @@
+// --------------------------------------------------------------------------------------
+// FAKE build script
+// --------------------------------------------------------------------------------------
+
+//#r "paket: group DotNetCoreBuild //"
+
+#r "paket:
+nuget Fake.Core
+nuget Fake.DotNet.Cli
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.Core.TargetOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core
+open Fake.DotNet
+
+// --------------------------------------------------------------------------------------
+// Build variables
+// --------------------------------------------------------------------------------------
+
+//let buildDir  = ".src//build/"
+
+let projectPath = !! "./src/Bson.FSharp" |> Seq.head
+let dotnetcliVersion = "2.0.2"
+let dotnetExePath = "dotnet"
+
+// --------------------------------------------------------------------------------------
+// Helpers
+// --------------------------------------------------------------------------------------
+
+let run' timeout cmd args dir =
+    if Process.execSimple (fun info ->
+        { info with 
+            FileName = cmd
+            Arguments = args
+            WorkingDirectory = if not (System.String.IsNullOrWhiteSpace dir) then dir else info.WorkingDirectory}
+    ) timeout <> 0 then
+        failwithf "Error while running '%s' with args: %s" cmd args
+
+let run = run' System.TimeSpan.MaxValue
+
+let runDotnet workingDir args =
+    let result =
+        Process.execSimple (fun info ->
+            { info with 
+                FileName = dotnetExePath
+                Arguments = args
+                WorkingDirectory = workingDir}) System.TimeSpan.MaxValue
+    if result <> 0 then failwithf "dotnet %s failed" args
+
+// --------------------------------------------------------------------------------------
+// Targets
+// --------------------------------------------------------------------------------------
+
+Target.create "Clean" (fun _ ->
+    let k = [ !! (projectPath + "/bin"); !! (projectPath + "/obj") ] |> Seq.map Seq.head |> Seq.toArray
+    printfn "Cleaning build dirs..."
+    k |> Seq.iter (fun dir -> 
+        printf "Cleaning dir %s" dir
+        Fake.IO.Shell.cleanDir dir
+        printfn " => Done")
+)
+
+Target.create "InstallDotNetCLI" (fun _ ->
+    DotNet.install 
+        (fun opts -> { opts with Version = DotNet.CliVersion.Version dotnetcliVersion }) 
+        (DotNet.Options.Create ()) 
+    |> ignore
+)
+
+Target.create "Restore" (fun _ ->
+    runDotnet projectPath "restore"
+)
+
+Target.create "Build" (fun param ->
+    match param.Context.Arguments with
+    | ["Debug"]
+    | ["Debug"; _] -> " -c Debug"
+    | []
+    | [_]
+    | ["Release"]
+    | ["Release"; _] -> " -c Release"
+    | _ -> failwith "Invalid arguments. Usage: Publish [Debug|Release] [ApiKey]"
+    |> sprintf "build%s"
+    |> runDotnet projectPath
+)
+
+Target.create "Pack" (fun param ->
+    match param.Context.Arguments with
+    | ["Debug"]
+    | ["Debug"; _] -> " -c Debug"
+    | []
+    | [_]
+    | ["Release"]
+    | ["Release"; _] -> " -c Release"
+    | _ -> failwith "Invalid arguments. Usage: Publish [Debug|Release] [ApiKey]"
+    |> sprintf "pack%s"
+    |> runDotnet projectPath
+)
+
+Target.create "Publish" (fun param ->
+    let key = 
+        match param.Context.Arguments with
+        | [key] -> key
+        | _ -> failwith "Invalid arguments. Usage: Publish ApiKey"
+    let package = !! (projectPath + "/bin/Release/*.nupkg") |> Seq.head
+    runDotnet "." (sprintf "nuget push %s -k %s -s https://api.nuget.org/v3/index.json" package key)
+)
+
+// --------------------------------------------------------------------------------------
+// Build order
+// --------------------------------------------------------------------------------------
+
+"Clean"
+    ==> "InstallDotNetCLI"
+    ==> "Restore"
+
+"InstallDotNetCLI"
+    ==> "Build"
+
+"InstallDotNetCLI"
+    ==> "Pack"
+    ==> "Publish"
+
+Target.runOrDefaultWithArguments "Build"
---
+++ src/Bson.FSharp/Serializers.fs
@@ -0,0 +1,94 @@
+namespace FSharpSerializers
+
+open MongoDB.Bson.Serialization
+open Microsoft.FSharp.Reflection
+open System
+open MongoDB.Bson.Serialization.Serializers
+open MongoDB.Bson
+
+type ListSerializer<'T>() =
+        inherit SerializerBase<'T list>()
+
+        override __.Serialize(context, _, value) =
+            let writer = context.Writer
+            writer.WriteStartArray ()
+            value |> List.iter (fun x -> BsonSerializer.Serialize (writer,typeof<'T>, x))
+            writer.WriteEndArray ()
+
+        override __.Deserialize(context, _) =
+            let reader = context.Reader
+
+            match reader.CurrentBsonType with
+            | BsonType.Null -> reader.ReadNull(); []
+            | BsonType.Array -> 
+                seq {
+                    reader.ReadStartArray ()
+                    while reader.ReadBsonType() <> BsonType.EndOfDocument do
+                        yield BsonSerializer.Deserialize<'T> (reader)
+                    reader.ReadEndArray ()
+                } |> Seq.toList
+            | bsonType -> 
+                sprintf "Can't deserialize a %s from BsonType %s" typeof<'T list>.FullName (bsonType.ToString())
+                |> InvalidOperationException
+                |> raise
+
+    type UnionCaseSerializer<'T>() =
+        inherit SerializerBase<'T>()
+
+        override __.Serialize(context, args, value) =
+            let writer = context.Writer
+            writer.WriteStartDocument ()
+            let info, values = FSharpValue.GetUnionFields (value, typeof<'T>)
+            writer.WriteName "_t"
+            writer.WriteString info.Name
+            writer.WriteName "_v"
+            writer.WriteStartArray ()
+            values |> Seq.zip (info.GetFields ()) |> Seq.iter (fun (field, value) ->
+                BsonSerializer.Serialize (writer, field.PropertyType, value)
+            )
+            writer.WriteEndArray ()
+            writer.WriteEndDocument()
+
+        override __.Deserialize(context, args) =
+            let reader = context.Reader
+
+            reader.ReadStartDocument()
+            let n = reader.ReadName (IO.Utf8NameDecoder.Instance)
+            if n = "_t" then
+                let typeName = reader.ReadString ()
+                let unionType = 
+                    FSharpType.GetUnionCases (typeof<'T>)
+                    |> Seq.where (fun case -> case.Name = typeName) |> Seq.head
+                reader.ReadStartArray ()
+                let items = 
+                    (unionType.GetFields () |> Seq.map (fun f -> f.PropertyType))
+                    |> Seq.map (fun t ->
+                        let serializer = BsonSerializer.LookupSerializer t
+                        serializer.Deserialize (context)
+                    ) |> Seq.toArray
+                reader.ReadEndArray()
+                reader.ReadEndDocument()
+                FSharpValue.MakeUnion (unionType, items) :?> 'T
+            else
+                failwith "No type information"
+
+    type FsharpSerializationProvider() =
+        interface IBsonSerializationProvider with
+            member __.GetSerializer(typ : Type) =
+                if FSharpType.IsUnion typ then
+                    if typ.IsGenericType && typ.GetGenericTypeDefinition() = typedefof<List<_>> then
+                         typedefof<ListSerializer<_>>.MakeGenericType(typ.GetGenericArguments())
+                         |> Activator.CreateInstance :?> IBsonSerializer
+                    else
+                        printfn "typ = %A" typ
+                        typedefof<UnionCaseSerializer<_>>.MakeGenericType([|typ|])
+                        |> Activator.CreateInstance :?> IBsonSerializer
+                else
+                    null
+
+module Registration =
+    
+    let register = lazy (
+            BsonSerializer.RegisterSerializationProvider(FsharpSerializationProvider())
+            BsonSerializer.RegisterGenericSerializerDefinition(typeof<list<_>>, typeof<ListSerializer<_>>))
+        
